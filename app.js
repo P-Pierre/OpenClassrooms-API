a6d7eeedce3a831727953d314ee1e303ba7c1830
pierre <pierreprie24@free.fr>
2023-03-24 16:43:09 +0100

    premier comit sur dev. Fin de la partie 1 du cours

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const express = require('express');
 const app = express();
 
 ////////// MIDDLEWARE //////////
+// au minimum le dernier objet de la chaine doit utiliser l'objet res pour renvoyer la réponse au client sinon la requete va expirer
+
+//intercepte les requetes qui contiennent du json et mets a disposition ce contenue du corps de la requete dans l'objet request via 'req.body'
+app.use(express.json());
 
 // utilise les objets 'req' : request et 'res' : response
 // la fonction next permet de renvoyer a une prochaine fonction midlewear l'exécution du server
+// app.use intercepte tout type de requete
 app.use((req,res, next) => {
     //Affiche un message dans la console 
     console.log('Requête recues !');
@@ -17,17 +22,52 @@ app.use((req,res, next) => {
     next();
 });
 
-app.use((req,res, next) => {
-    //Modifie le status du message de réponse
-    res.status(201);
+//Pas d'addresse spécifié, donc appliqué a toute les routes
+//La définition se fait sur l'objet réponse
+app.use((req, res, next) => {
+    //autorise l'accès a l'API depuis toute les origines
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    //autorise certains headers sur les requêtes a notre API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    //autorise certains méthodes sur les requêtes a notre API
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+// app.post intercepte uniquement les requetes POST
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    //Modifie le status du message de réponse et l'objet json du message retourné
+    res.status(201).json({message: 'Objet crée l'});
     next();
 });
 
-app.use((req,res, next) => {
-    //renvoyer une reponse en json
-    res.json({message: 'Vore requête a bien été recue !'});
+
+//on spécifie en plusle chemin pour lequel la fonction Middleware est utilisé
+// app.get intercepte uniquement les requetes GET
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+
+    res.status(200).json(stuff);
     next();
-});
+  });
 
 app.use((req,res, next) => {
     console.log('Reponse envoyé avec succes !');
